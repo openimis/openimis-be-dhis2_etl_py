2936cef3a3c9974f4744370a07a245c7a68475f5
Patrick Delcroix <patrick.delcroix@swisstph.ch>
2021-04-01 07:27:37 +0000

    fix: claim event dataelement not pushed

--- dhis2_pusher/converters/ClaimConverter.py
+++ dhis2_pusher/converters/ClaimConverter.py
@@ -6,7 +6,7 @@ from product.models import ProductItem, ProductService
 from .. import models
 from . import BaseDHIS2Converter
 from ..configurations import GeneralConfiguration
-from dhis2 import utils
+from dhis2.utils import *
 import hashlib 
 from ..models.dhis2Program import *
 from ..utils import toDateStr, toDatetimeStr, build_dhis2_id
@@ -23,7 +23,7 @@ class ClaimConverter(BaseDHIS2Converter):
 
 
     @classmethod
-    def to_enrollment_obj(cls, claim, event = False ):
+    def to_enrollment_obj(cls, claim, event = False , **kwargs):
         if claim is not None and claim.insuree is not None and claim.insuree.uuid is not None:
             trackedEntity = build_dhis2_id(claim.insuree.uuid)
             uid = build_dhis2_id(claim.uuid)
@@ -38,25 +38,25 @@ class ClaimConverter(BaseDHIS2Converter):
                     attributes.append(AttributeValue( attribute = claimProgram['attributes']['claimNumber'],\
                     value = claim.code)) 
             #    "diagnoseMain"
-            if claim.icd is not None and is_valid_uid(claimProgram['attributes']['diagnoseMain']):
+            if claim.icd_id is not None and is_valid_uid(claimProgram['attributes']['diagnoseMain']):
                     attributes.append(AttributeValue( attribute = claimProgram['attributes']['diagnoseMain'],\
-                    value = claim.icd.code + " - " + claim.icd.name))
+                    value = claim.icd_id))
             #    "diagnoseSec1"
-            if claim.icd_1 is not None and is_valid_uid(claimProgram['attributes']['diagnoseSec1']):
+            if claim.icd_1_id is not None and is_valid_uid(claimProgram['attributes']['diagnoseSec1']):
                     attributes.append(AttributeValue( attribute = claimProgram['attributes']['diagnoseSec1'],\
-                    value = claim.icd_1.code + " - " + claim.icd_1.name))
+                    value = claim.icd_1_id))
             #    "diagnoseSec2"
-            if claim.icd_2 is not None and is_valid_uid(claimProgram['attributes']['diagnoseSec2']):
+            if claim.icd_2_id is not None and is_valid_uid(claimProgram['attributes']['diagnoseSec2']):
                     attributes.append(AttributeValue( attribute = claimProgram['attributes']['diagnoseSec2'],\
-                    value = claim.icd_2.code + " - " + claim.icd_2.name))
+                    value = claim.icd_2_id))
             #    "diagnoseSec3"
-            if claim.icd_3 is not None and is_valid_uid(claimProgram['attributes']['diagnoseSec3']):
+            if claim.icd_3_id is not None and is_valid_uid(claimProgram['attributes']['diagnoseSec3']):
                     attributes.append(AttributeValue( attribute = claimProgram['attributes']['diagnoseSec3'],\
-                    value = claim.icd_3.code + " - " + claim.icd_3.name))
+                    value = claim.icd_3_id))
             #    "diagnoseSec4"
-            if claim.icd_4 is not None and is_valid_uid(claimProgram['attributes']['diagnoseSec4']):
+            if claim.icd_4_id is not None and is_valid_uid(claimProgram['attributes']['diagnoseSec4']):
                     attributes.append(AttributeValue( attribute = claimProgram['attributes']['diagnoseSec4'],\
-                    value = claim.icd_4.code + " - " + claim.icd_4.name))
+                    value = claim.icd_4_id))
             #    "VisitType"
             if claim.visit_type is not None and is_valid_uid(claimProgram['attributes']['VisitType']):
                     attributes.append(AttributeValue( attribute = claimProgram['attributes']['VisitType'],\
@@ -81,7 +81,7 @@ class ClaimConverter(BaseDHIS2Converter):
 
  
     @classmethod
-    def to_enrollment_objs(cls, claims, event = False):
+    def to_enrollment_objs(cls, claims, event = False, **kwargs):
         Enrollments = []
         for claim in claims:
             Enrollments.append(cls.to_enrollment_obj(claim, event))
@@ -92,30 +92,31 @@ class ClaimConverter(BaseDHIS2Converter):
         # add claim details event
         stageDE = claimProgram['stages']['claimDetails']['dataElements']
         orgUnit = build_dhis2_id(claim.health_facility.uuid)
-        trackedEntityInstance = build_dhis2_id(claim.uuid)
-        dataValue = []
+        trackedEntityInstance = build_dhis2_id(claim.insuree.uuid)
+        dataValues = []
         # "status"
         if is_valid_uid(stageDE['status']):
-            dataValue.append(EventDataValue(dataElement = stageDE['status'],\
+            dataValues.append(EventDataValue(dataElement = stageDE['status'],\
                 value = GeneralConfiguration.get_claim_status_code(claim.status)))
         # "amount"
         if is_valid_uid(stageDE['amount']):
-            dataValue.append(EventDataValue(dataElement = stageDE['amount'], value = claim.claimed ))
+            dataValues.append(EventDataValue(dataElement = stageDE['amount'], value = claim.claimed ))
         # "checkedDate"
         if is_valid_uid(stageDE['checkedDate']):
-                dataValue.append(EventDataValue(dataElement = stageDE['checkedDate'], value = toDateStr(claim.date_claimed) ))
+            dataValues.append(EventDataValue(dataElement = stageDE['checkedDate'],\
+                 value = toDateStr(claim.date_claimed) ))
         # "processedDate"
         if is_valid_uid(stageDE['processedDate']) and claim.process_stamp is not None:
-            dataValue.append(EventDataValue(dataElement = stageDE['processedDate'],\
+            dataValues.append(EventDataValue(dataElement = stageDE['processedDate'],\
                 value = toDateStr(claim.process_stamp)))
         # "adjustedDate"
         if is_valid_uid(stageDE['adjustedDate']) and claim.submit_stamp is not None:
-            dataValue.append(EventDataValue(dataElement = stageDE['adjustedDate'], \
+            dataValues.append(EventDataValue(dataElement = stageDE['adjustedDate'], \
                 value = toDateStr(claim.submit_stamp)))
         if (claim.status == CLAIM_VALUATED or claim.status == CLAIM_PROCESSED):
             # "adjustedAmount"
             if is_valid_uid(stageDE['adjustedAmount']) and claim.valuated is not None :
-                dataValue.append(EventDataValue(dataElement = stageDE['adjustedAmount'],\
+                dataValues.append(EventDataValue(dataElement = stageDE['adjustedAmount'],\
                     value = claim.valuated ))
             # "valuationDate" # FIXME not correct in case of batch run
             if is_valid_uid(stageDE['valuationDate']):
@@ -123,19 +124,19 @@ class ClaimConverter(BaseDHIS2Converter):
                     dateEvent = claim.process_stamp.date()
                 else:
                     dateEvent = claim.submit_stamp.date()
-                dataValue.append(EventDataValue(dataElement = stageDE['valuationDate'], \
+                dataValues.append(EventDataValue(dataElement = stageDE['valuationDate'], \
                     value =  toDateStr(dateEvent)))
             # "approvedAmount":"TiZrzsT8088",
             if is_valid_uid(stageDE['approvedAmount']) and claim.approved is not None:
-                dataValue.append(EventDataValue(dataElement = stageDE['approvedAmount'], \
+                dataValues.append(EventDataValue(dataElement = stageDE['approvedAmount'], \
                     value = claim.approved ))
             # "valuatedAmount":"Fk7sSgbFTaG",
             if is_valid_uid(stageDE['valuatedAmount']) and claim.valuated is not None:
-                dataValue.append(EventDataValue(dataElement = stageDE['valuatedAmount'], \
+                dataValues.append(EventDataValue(dataElement = stageDE['valuatedAmount'], \
                     value = claim.valuated))
             # "renumeratedAmount":""
-            if is_valid_uid(stageDE['renumeratedAmount'])and claim.reinsured is not None:
-                dataValue.append(EventDataValue(dataElement = stageDE['renumeratedAmount'], \
+            if is_valid_uid(stageDE['renumeratedAmount']) and claim.reinsured is not None:
+                dataValues.append(EventDataValue(dataElement = stageDE['renumeratedAmount'], \
                     value = claim.reinsured))
         # "rejectionDate"
         elif (claim.status == CLAIM_REJECTED ) and is_valid_uid(stageDE['rejectionDate']):
@@ -143,16 +144,15 @@ class ClaimConverter(BaseDHIS2Converter):
                 dateEvent = claim.process_stamp.date()
             else:
                 dateEvent = claim.submit_stamp.date()
-
-
-            dataValue.append(EventDataValue(dataElement = stageDE['rejectionDate'],\
+            dataValues.append(EventDataValue(dataElement = stageDE['rejectionDate'],\
                 value = toDateStr(dateEvent)))
         return Event(\
+            event =  build_dhis2_id(claim.uuid),\
             program = claimProgram['id'],\
             orgUnit = orgUnit,\
             eventDate = toDateStr(claim.date_from) ,\
             status = "COMPLETED",\
-            dataValue = dataValue,\
+            dataValues = dataValues,\
             trackedEntityInstance = trackedEntityInstance,\
             programStage = claimProgram['stages']['claimDetails']['id'])
 
@@ -160,110 +160,120 @@ class ClaimConverter(BaseDHIS2Converter):
 
 
     @classmethod
-    def to_event_item_obj(cls, item, claim = None):
+    def to_event_item_obj(cls, item, claim = None, **kwargs):
         orgUnit = build_dhis2_id(claim.health_facility.uuid)
         trackedEntityInstance = build_dhis2_id(claim.insuree.uuid)
         stageDE = claimProgram['stages']['items']['dataElements']
-        dataValue = []
+        dataValues = []
         #"item":"VFWCqLKPuSd",
-        if is_valid_uid(stageDE['item'])and item.item is not None:
-                dataValue.append(EventDataValue(dataElement = stageDE['item'], \
+        if is_valid_uid(stageDE['item']) and item.item is not None:
+            dataValues.append(EventDataValue(dataElement = stageDE['item'], \
                     value = item.item.code + " - " + item.item.name))
         #"quantity":"xBdXypAmk7V", # 
-        if is_valid_uid(stageDE['quantity'])and item.qty_provided is not None:
-                dataValue.append(EventDataValue(dataElement = stageDE['quantity'], \
+        if is_valid_uid(stageDE['quantity']) and item.qty_provided is not None:
+            dataValues.append(EventDataValue(dataElement = stageDE['quantity'], \
                     value = item.qty_provided)) 
         #"price":"Gu1DbTMoVGx",
-        if is_valid_uid(stageDE['price'])and item.price_asked is not None:
-                dataValue.append(EventDataValue(dataElement = stageDE['price'], \
+        if is_valid_uid(stageDE['price']) and item.price_asked is not None:
+            dataValues.append(EventDataValue(dataElement = stageDE['price'], \
                     value = item.price_asked))
         #"deductibleAmount":"uWJD6i5xf6A",
-        if is_valid_uid(stageDE['deductibleAmount'])and item.deductable_amount is not None:
-                dataValue.append(EventDataValue(dataElement = stageDE['deductibleAmount'], \
+        if is_valid_uid(stageDE['deductibleAmount']) and item.deductable_amount is not None:
+            dataValues.append(EventDataValue(dataElement = stageDE['deductibleAmount'], \
                     value = item.deductable_amount ))
         #"exeedingCeilingAmount":"krBi9DbQl4Y",
-        if is_valid_uid(stageDE['exeedingCeilingAmount'] )and item.exceed_ceiling_amount is not None:
-                dataValue.append(EventDataValue(dataElement = stageDE['exeedingCeilingAmount'] , \
+        if is_valid_uid(stageDE['exeedingCeilingAmount'] ) and item.exceed_ceiling_amount is not None:
+            dataValues.append(EventDataValue(dataElement = stageDE['exeedingCeilingAmount'] , \
                     value = item.exceed_ceiling_amount ))
         #"renumeratedAmount":"WyAw53dfnMj", # not used
-        if is_valid_uid(stageDE['renumeratedAmount'])and item.remunerated_amount is not None:
-                dataValue.append(EventDataValue(dataElement = stageDE['renumeratedAmount'], \
+        if is_valid_uid(stageDE['renumeratedAmount']) and item.remunerated_amount is not None:
+            dataValues.append(EventDataValue(dataElement = stageDE['renumeratedAmount'], \
                     value = item.remunerated_amount ))
         #"seqId":"QmuynKAhycW" # same Service
-        #if is_valid_uid(stageDE['renumeratedAmount'])and item.reinsured is not None:
-        #        dataValue.append(EventDataValue(dataElement = stageDE['renumeratedAmount'], \
+        #if is_valid_uid(stageDE['renumeratedAmount']) and item.reinsured is not None:
+        #        dataValues.append(EventDataValue(dataElement = stageDE['renumeratedAmount'], \
         #            item.reinsured))     
         return Event(\
+            event =  build_dhis2_id(item.id, 'claimItem'),\
             program = claimProgram['id'],\
             orgUnit = orgUnit,\
             eventDate = toDateStr(claim.date_from) ,\
             status = "COMPLETED",\
-            dataValue = dataValue,\
+            dataValues = dataValues,\
             trackedEntityInstance = trackedEntityInstance,\
             programStage = claimProgram['stages']['items']['id'])
            
 
     @classmethod
-    def to_event_service_obj(cls, service, claim = None):
+    def to_event_service_obj(cls, service, claim = None, **kwargs):
         
         trackedEntityInstance = build_dhis2_id(claim.insuree.uuid)
         orgUnit = build_dhis2_id(claim.health_facility.uuid)
         stageDE = claimProgram['stages']['services']['dataElements']
-        dataValue = []
+        dataValues = []
         #"adjustedAmount"not used
-        if is_valid_uid(stageDE['adjustedAmount'])and service.price_adjusted is not None:
-                dataValue.append(EventDataValue(dataElement = stageDE['adjustedAmount'], \
+        if is_valid_uid(stageDE['adjustedAmount']) and service.price_adjusted is not None:
+            dataValues.append(EventDataValue(dataElement = stageDE['adjustedAmount'], \
                     value = service.price_adjusted  ))
         #"approvedAmount" # not used
-        if is_valid_uid(stageDE['approvedAmount'])and service.price_approved is not None:
-                dataValue.append(EventDataValue(dataElement = stageDE['approvedAmount'], \
+        if is_valid_uid(stageDE['approvedAmount']) and service.price_approved is not None:
+            dataValues.append(EventDataValue(dataElement = stageDE['approvedAmount'], \
                     value = service.price_approved  ))
         #"valuatedAmount" # not used
-        if is_valid_uid(stageDE['valuatedAmount'])and service.remunerated_amount is not None:
-                dataValue.append(EventDataValue(dataElement = stageDE['valuatedAmount'], \
+        if is_valid_uid(stageDE['valuatedAmount']) and service.remunerated_amount is not None:
+            dataValues.append(EventDataValue(dataElement = stageDE['valuatedAmount'], \
                     value = service.remunerated_amount ))
         #"service":
-        if is_valid_uid(stageDE['service'])and service.service is not None:
-                dataValue.append(EventDataValue(dataElement = stageDE['service'], \
+        if is_valid_uid(stageDE['service']) and service.service is not None:
+            dataValues.append(EventDataValue(dataElement = stageDE['service'], \
                     value = service.service.code + " - " + service.service.name))
         #"quantity":,
-        if is_valid_uid(stageDE['quantity'])and service.qty_provided is not None:
-                dataValue.append(EventDataValue(dataElement = stageDE['quantity'], \
+        if is_valid_uid(stageDE['quantity']) and service.qty_provided is not None:
+            dataValues.append(EventDataValue(dataElement = stageDE['quantity'], \
                     value = service.qty_provided)) 
         #"price":"uwGg814hDhB",
-        if is_valid_uid(stageDE['price'])and service.price_asked is not None:
-                dataValue.append(EventDataValue(dataElement = stageDE['price'], \
+        if is_valid_uid(stageDE['price']) and service.price_asked is not None:
+            dataValues.append(EventDataValue(dataElement = stageDE['price'], \
                     value = service.price_asked))
         #"deductibleAmount"
-        if is_valid_uid(stageDE['deductibleAmount'])and service.deductable_amount is not None:
-                dataValue.append(EventDataValue(dataElement = stageDE['deductibleAmount'], \
+        if is_valid_uid(stageDE['deductibleAmount']) and service.deductable_amount is not None:
+            dataValues.append(EventDataValue(dataElement = stageDE['deductibleAmount'], \
                     value = service.deductable_amount ))
         #"exeedingCeilingAmount"
-        if is_valid_uid(stageDE['exeedingCeilingAmount'] )and service.exceed_ceiling_amount is not None:
-                dataValue.append(EventDataValue(dataElement = stageDE['exeedingCeilingAmount'] , \
+        if is_valid_uid(stageDE['exeedingCeilingAmount'] ) and service.exceed_ceiling_amount is not None:
+            dataValues.append(EventDataValue(dataElement = stageDE['exeedingCeilingAmount'] , \
                     value = service.exceed_ceiling_amount ))
         #"renumeratedAmount": # not used
-        if is_valid_uid(stageDE['renumeratedAmount'])and service.remunerated_amount is not None:
-                dataValue.append(EventDataValue(dataElement = stageDE['renumeratedAmount'], \
+        if is_valid_uid(stageDE['renumeratedAmount']) and service.remunerated_amount is not None:
+            dataValues.append(EventDataValue(dataElement = stageDE['renumeratedAmount'], \
                     value = service.remunerated_amount ))
         #"seqId":"QmuynKAhycW"
-        #if is_valid_uid(stageDE['renumeratedAmount'])and service.reinsured is not None:
-        #        dataValue.append(EventDataValue(dataElement = stageDE['renumeratedAmount'], \
+        #if is_valid_uid(stageDE['renumeratedAmount']) and service.reinsured is not None:
+        #        dataValues.append(EventDataValue(dataElement = stageDE['renumeratedAmount'], \
         #            service.reinsured)) 
         return Event(\
+            event =  build_dhis2_id(service.id, 'claimService'),\
             program = claimProgram['id'],\
             orgUnit = orgUnit,\
             eventDate = toDateStr(claim.date_from) ,\
             status = "COMPLETED",\
-            dataValue = dataValue,\
+            dataValues = dataValues,\
             trackedEntityInstance = trackedEntityInstance,\
             programStage = claimProgram['stages']['services']['id'])
         
         
     @classmethod
-    def to_event_objs(cls, claims):
-        Enrollments =[]
+    def to_event_objs(cls, claims, **kwargs):
+        events =[]
         for claim in claims:
-            Enrollments.append(cls.to_enrollment_obj(claim))
-        return EventBundle(Enrollments)
+            if claimProgram['stages']['claimDetails'] is not None :
+                    events.append(cls.to_event_obj(claim)) # add claim details
+            if claimProgram['stages']['services'] is not None :
+                for service in claim.services.all(): 
+                    events.append(cls.to_event_service_obj(service, claim = claim)) # add claim items
+            if claimProgram['stages']['items'] is not None:
+                for item in claim.items.all():
+                    events.append(cls.to_event_item_obj(item, claim = claim)) # add claim service
+
+        return EventBundle(events = events)
 
--- dhis2_pusher/services/claimServices.py
+++ dhis2_pusher/services/claimServices.py
@@ -35,13 +35,26 @@ def syncClaim(startDate,stopDate):
             .select_related('insuree')\
             .select_related('admin')\
             .select_related('health_facility')\
-            .select_related('icd')\
-            .select_related('icd_1')\
-            .select_related('icd_2')\
-            .select_related('icd_3')\
-            .select_related('icd_4')\
             .prefetch_related(Prefetch('items', queryset=ClaimItem.objects.filter(validity_to__isnull=True).select_related('item')))\
             .prefetch_related(Prefetch('services', queryset=ClaimService.objects.filter(validity_to__isnull=True).select_related('service')))\
             .order_by('validity_from')
     # get the insuree matching the search
     return postMethod('enrollments',claims, ClaimConverter.to_enrollment_objs, event = True)
+
+   
+def syncClaimEvent(startDate,stopDate):
+    # get only the last version of valudated or rejected claims (to sending multiple time the same claim)
+    claims = Claim.objects.filter(validity_to__isnull=True)\
+            .filter(validity_from__lte=stopDate)\
+            .filter(validity_from__gte=startDate)\
+            .filter(insuree__legacy_id__isnull=True)\
+            .filter(Q(status=CLAIM_VALUATED)| Q(status=CLAIM_REJECTED))\
+            .order_by('validity_from')\
+            .select_related('insuree')\
+            .select_related('admin')\
+            .select_related('health_facility')\
+            .prefetch_related(Prefetch('items', queryset=ClaimItem.objects.filter(validity_to__isnull=True).select_related('item')))\
+            .prefetch_related(Prefetch('services', queryset=ClaimService.objects.filter(validity_to__isnull=True).select_related('service')))\
+            .order_by('validity_from')
+    # get the insuree matching the search
+    return postMethod('events',claims, ClaimConverter.to_event_objs, event = True)
