58c056a13c9c6e84d6a54e6f0228dae7a6486b24
delcroip <patrick.delcroix@swisstph.ch>
2023-11-26 11:51:06 +0100

    fixing prod cat

--- dhis2_etl/services/adx/data_values.py
+++ dhis2_etl/services/adx/data_values.py
@@ -54,7 +54,7 @@ def get_location_contribution_sum_dv(period):
             *filter_validity(), 
             policy__family__location__parent=l),
         aggregation_func= Sum('amount'),
-        categories=[get_product_categories(period, prefix='policy__')]
+        categories=[get_product_categories(period)]
     )
 
 
@@ -62,10 +62,10 @@ def get_hf_claim_number_dv(period):
     return ADXMappingDataValueDefinition(
         data_element="NB_CLAIM",
         period_filter_func=get_claim_period_filter,
-        dataset_from_orgunit_func=lambda hf: hf.claim_set.filter(*filter_validity()).annotate(product= Coalesce(Max('services__policy__product'),Max('items__policy__product'))),
+        dataset_from_orgunit_func=lambda hf: hf.claim_set.filter(*filter_validity()).annotate(product_code= Coalesce(Max('services__policy__product__code'),Max('items__policy__product__code'))),
         aggregation_func=Count('id'),
         categories=[
-            get_product_categories(period),
+            get_product_categories(period, prefix='product_'),
             get_claim_status_categories(),
             get_claim_type_categories(),
             get_sex_categories(prefix='insuree__'),
@@ -84,7 +84,7 @@ def get_hf_claim_item_number_dv(period):
             *filter_validity(prefix='claim__')).annotate(qty=Coalesce('qty_approved', 'qty_provided')),
         aggregation_func=Sum( 'qty'),
         categories=[
-            get_product_categories(period, prefix='policy__'),
+            get_product_categories(period),
             get_claim_status_categories(prefix='claim__'),
             get_claim_details_status_categories(),
             get_claim_type_categories(prefix='claim__'),
@@ -105,7 +105,7 @@ def get_hf_claim_service_number_dv(period):
             *filter_validity(prefix='claim__')).annotate(qty=Coalesce('qty_approved', 'qty_provided')),
         aggregation_func=Sum('qty'),
         categories=[
-            get_product_categories(period, prefix='policy__'),
+            get_product_categories(period),
             get_claim_status_categories(prefix='claim__'),
             get_claim_details_status_categories(),
             get_claim_type_categories(prefix='claim__'),
@@ -125,7 +125,7 @@ def get_hf_claim_service_number_icd_dv(period):
             *filter_validity(prefix='claim__')).annotate(qty=Coalesce('qty_approved', 'qty_provided')),
         aggregation_func=Sum('qty'),
         categories=[
-            get_product_categories(period, prefix='policy__'),
+            get_product_categories(period),
             get_claim_status_categories(prefix='claim__'),
             get_claim_details_status_categories(),
             get_main_icd_categories(period, prefix='claim__')
@@ -143,7 +143,7 @@ def get_hf_claim_services_valuated_dv(period):
             claim__date_processed__isnull=True),
         aggregation_func=Sum('price_valuated'),
         categories=[
-            get_product_categories(period, prefix='policy__'),
+            get_product_categories(period),
             get_claim_status_categories(prefix='claim__'),
             get_claim_details_status_categories(),
             get_claim_type_categories(prefix='claim__'),
@@ -166,7 +166,7 @@ def get_hf_claim_service_asked_dv(period):
             qty_provided__gte=0 ).annotate(full_price=F('price_asked') * F('qty_provided')),
         aggregation_func=Sum('full_price'),
         categories=[
-            get_product_categories(period, prefix='policy__'),
+            get_product_categories(period),
             get_claim_status_categories(prefix='claim__'),
             get_claim_service_categories(),
             get_sex_categories(prefix='claim__insuree__'),
